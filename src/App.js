3ab7e68d09f53783cb92fbc542d363561c2af9ea
Ankita Shah Ashwin <ankita.shah.ashwin@ipk.fraunhofer.de>
2020-06-09 21:21:04 +0200

    basic navbar setup

--- src/App.js
+++ src/App.js
@@ -2,9 +2,42 @@ import React from "react";
 
 const App = ({ title }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-    </div>
+    <section className="main-section">
+      <section>
+        <header className="nav-header">
+          <span className="nav-section">
+            <ul className="navigation">
+              <a>
+                <img
+                  className="fela-_18vovew"
+                  src="https://cdn.hellofresh.com/logo/HelloFresh_Logo_Horizontal_V2.svg"
+                  alt="HelloFresh"
+                  data-test-id="desktop-hellofresh-logo"
+                />
+              </a>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Demo</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <button>Login</button>
+              </li>
+            </ul>
+          </span>
+        </header>
+      </section>
+    </section>
   );
 };
 
