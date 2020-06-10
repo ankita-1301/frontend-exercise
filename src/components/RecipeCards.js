7d668b0dc958364585f4a9d5029b957c11756c77
Ankita Shah Ashwin <ankita.shah.ashwin@ipk.fraunhofer.de>
2020-06-10 17:03:28 +0200

    add receipe details to the card with styöes

--- src/components/RecipeCards.js
+++ src/components/RecipeCards.js
@@ -3,6 +3,11 @@ import recipeData from "../../recipes.json";
 
 const RecipeCards = (props) => {
   console.log(recipeData);
+  const strongStyle = {
+    textTransform: "uppercase",
+    letterSpacing: "1px",
+    color: "black",
+  };
   return (
     <div className="grid-container">
       {recipeData.map((recipe) => {
@@ -12,7 +17,32 @@ const RecipeCards = (props) => {
               src="https://eatforum.org/content/uploads/2018/05/table_with_food_top_view_900x700.jpg"
               alt="Avatar"
             />
-            <h2>{recipe.name}</h2>
+            <div className="card-details">
+              <h3
+                style={{
+                  paddingBottom: 0,
+                  marginTop: "10px",
+                  lineheight: "0.4em",
+                }}
+              >
+                {recipe.name}
+              </h3>
+              <h5 style={{ margin: 0, padding: 0 }}>{recipe.headline}</h5>
+              <hr />
+              <p>
+                <strong style={strongStyle}>Ingredients:</strong>
+                {recipe.ingredients + "  "}
+              </p>
+              <p>
+                <strong style={strongStyle}>Calories:</strong> {recipe.calories}
+              </p>
+              <p>
+                <strong style={strongStyle}>Time:</strong> {recipe.time}
+              </p>
+              <p>
+                <strong style={strongStyle}>Proteins:</strong> {recipe.proteins}
+              </p>
+            </div>
           </div>
         );
       })}
