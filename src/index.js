a7ca893da2b9926c99fab224cd84e5865da6742d
Ankita Shah Ashwin <ankita.shah.ashwin@ipk.fraunhofer.de>
2020-06-10 12:57:17 +0200

    replace BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/app.less";
-import HomePage from "./components/HomePage";
-import { Route } from "react-router";
-
-const title = "My Feel-Fresh Application";
 
 render(
-  <BrowserRouter>
-    <App title={title} />
-    <Route exact path="/" component={HomePage} />
-  </BrowserRouter>,
+  <HashRouter>
+    <App />
+  </HashRouter>,
   document.getElementById("app")
 );
 
