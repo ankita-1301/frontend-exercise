4a8f4fd6abea77225ea0315829c33a173187c410
Ankita Shah Ashwin <ankita.shah.ashwin@ipk.fraunhofer.de>
2020-06-10 13:00:19 +0200

    change anchor tags to router Links in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,57 +1,32 @@
 import React from "react";
-// import "../styles/app.less";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const NavBar = (props) => {
+const NavBar = () => {
   return (
-    <section>
-      <header className="nav-header">
-        <span className="nav-section">
-          <ul className="navigation">
-            <a>
-              <img
-                className="fela-_18vovew"
-                src="https://cdn.hellofresh.com/logo/HelloFresh_Logo_Horizontal_V2.svg"
-                alt="HelloFresh"
-                data-test-id="desktop-hellofresh-logo"
-              />
-            </a>
-            <li>
-              <NavLink to="/">
-                <a href="/">Home</a>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/">
-                <a href="/">About</a>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/">
-                <a href="/">Blog</a>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/">
-                <a href="/">Contact</a>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/">
-                <a href="/">About</a>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/login">
-                <a href="/login">
-                  <button>Login</button>
-                </a>
-              </NavLink>
-            </li>
-          </ul>
-        </span>
-      </header>
-    </section>
+    <ul className="header">
+      <li className="logo">
+        <img
+          className="hellofresh-logo"
+          src="https://cdn.hellofresh.com/logo/HelloFresh_Logo_Horizontal_V2.svg"
+          alt="HelloFresh"
+        />
+      </li>
+      <li>
+        <Link to={`/`}>Home</Link>
+      </li>
+      <li>
+        <Link to={`/about`}>About</Link>
+      </li>
+      <li>
+        <Link to={`/blog`}>Blog</Link>
+      </li>
+      <li>
+        <Link to={`/career`}>Career</Link>
+      </li>
+      <li className="right">
+        <button>Login</button>
+      </li>
+    </ul>
   );
 };
 
