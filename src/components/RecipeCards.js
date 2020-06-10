6a29bc16f87cd06c49c6f906d03049c70086e404
Ankita Shah Ashwin <ankita.shah.ashwin@ipk.fraunhofer.de>
2020-06-10 18:54:57 +0200

    favourite receipes in ReceipeCards

--- src/components/RecipeCards.js
+++ src/components/RecipeCards.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import recipeData from "../../recipes.json";
 
 const RecipeCards = (props) => {
-  console.log(recipeData);
+  const [favourite, setFavourite] = useState([]);
+  const [recipes] = useState(recipeData);
+
   const strongStyle = {
     textTransform: "uppercase",
     letterSpacing: "1px",
     color: "black",
   };
+  const fontStyle = {
+    fontSize: "28px",
+    float: "right",
+    margin: "17px 23px 0px 0px",
+    paddingBottom: "79px",
+    cursor: "pointer",
+  };
+
+  const onClickFavourite = (event) => {
+    let exists = favourite.filter((r) => r.id === event.id).length;
+    if (!exists) {
+      setFavourite([...favourite, ...[event]]);
+    } else {
+      setFavourite([...favourite.filter((r) => r.id !== event.id)]);
+    }
+  };
+
   return (
     <div className="grid-container">
-      {recipeData.map((recipe) => {
+      {recipes.map((recipe) => {
         return (
           <div className="card" key={recipe.id}>
             <img
@@ -18,6 +37,20 @@ const RecipeCards = (props) => {
               alt="Avatar"
             />
             <div className="card-details">
+              {favourite.includes(recipe) ? (
+                <i
+                  className="fas fa-heart"
+                  style={fontStyle}
+                  onClick={() => onClickFavourite(recipe)}
+                ></i>
+              ) : (
+                <i
+                  className="far fa-heart"
+                  style={fontStyle}
+                  onClick={() => onClickFavourite(recipe)}
+                ></i>
+              )}
+
               <h3
                 style={{
                   paddingBottom: 0,
@@ -27,6 +60,7 @@ const RecipeCards = (props) => {
               >
                 {recipe.name}
               </h3>
+
               <h5 style={{ margin: 0, padding: 0 }}>{recipe.headline}</h5>
               <hr />
               <p>
