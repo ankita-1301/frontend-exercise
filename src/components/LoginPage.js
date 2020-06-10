c40903aaf48470b3720d85e9a8114a6238a6b4b8
Ankita Shah Ashwin <ankita.shah.ashwin@ipk.fraunhofer.de>
2020-06-10 15:24:08 +0200

    LoginPage component with styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,8 +2,40 @@ import React from "react";
 
 const LoginPage = (props) => {
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="login-container-div">
+      <form className="login-form">
+        <div class="login-container">
+          <h1>Login</h1>
+          <hr />
+          <label for="email">
+            <b>Email</b>
+          </label>
+          <input
+            type="text"
+            placeholder="eg. username@hotmail.com"
+            name="email"
+            id="email"
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+            required
+          />
+          <label for="psw">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="password"
+            name="psw"
+            id="psw"
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            title="Must contain at least one  number and one uppercase and lowercase letter, and at least 8 or more characters"
+            required
+          />
+          <hr />
+          <button type="submit" class="registerbtn">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
