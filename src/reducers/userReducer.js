62f302c2ee10ddd483ad241afa5e54e1f968f9bd
Ankita Shah Ashwin <ankita.shah.ashwin@ipk.fraunhofer.de>
2020-06-13 20:56:07 +0200

    eliminate switch statement userReducer as there is just one action

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,14 +1,9 @@
 let initialState = false;
 
 const userReducer = (state = initialState, action) => {
-  switch (
-    action.type //switch used so that in future if there are many action.type then the code can be reused
-  ) {
-    case "LOGIN_LOGOUT":
-      return !state;
-    default:
-      return state;
-  }
+  if (action.type == "LOGIN_LOGOUT") {
+    return !state;
+  } else return state;
 };
 
 export default userReducer;
