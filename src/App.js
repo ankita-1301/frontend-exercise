8b056de316354d4d9676389523d30936b0163264
Ankita Shah Ashwin <ankita.shah.ashwin@ipk.fraunhofer.de>
2020-06-13 12:43:49 +0200

    remove unwanted div from app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-// import "../styles/app.less";
 import LoginPage from "./components/LoginPage";
 import { Route, Switch } from "react-router";
 import HomePage from "./components/HomePage";
@@ -9,12 +8,10 @@ const App = () => {
   return (
     <div>
       <NavBar />
-      <div className="home-page">
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/login" component={LoginPage} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/login" component={LoginPage} />
+      </Switch>
     </div>
   );
 };
