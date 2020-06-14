e2cef9f5cbaa9c7417892f2677e7ec3b9a9cc615
Ankita Shah Ashwin <ankita.shah.ashwin@ipk.fraunhofer.de>
2020-06-14 19:12:27 +0200

    fix className bug

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
         <Route exact path="/" component={HomePage} />
         <Route exact path="/login" component={LoginPage} />
       </Switch>
-      <div class="footer">
+      <div className="footer">
         <p>Hello Fresh is the place to be!</p>
       </div>
     </div>
